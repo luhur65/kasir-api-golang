480db4086a5f48379bcf10dd4a69f798155ba9f7
Dharma Situmorang <dharmabakti1202@gmail.com>
2026-02-10 21:43:49 +0700

    feat: update CORS middleware to allow dynamic origin and enhance allowed headers

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -7,13 +7,19 @@ import(
 func CORS(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		// Header CORS
-		w.Header().Set("Access-Control-Allow-Origin", "*") 
+		origin := r.Header.Get("Origin")
+		if origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
+		w.Header().Set("Vary", "Origin")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "X-API-KEY")
+		w.Header().Set(
+			"Access-Control-Allow-Headers",
+			"Content-Type, Authorization, X-API-KEY",
+		)
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
-		// Handle preflight request
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusNoContent)
 			return
@@ -22,3 +28,4 @@ func CORS(next http.HandlerFunc) http.HandlerFunc {
 		next.ServeHTTP(w, r)
 	})
 }
+
